702972ab2f487da40f95184a49b6d75c8d120e99
YURA BUYLOV <yubuylov@gmail.com>
2019-07-12 18:14:30 +0300

    2xx status acceptable

--- request.go
+++ request.go
@@ -30,7 +30,7 @@ func processResponse(response *http.Response) ([]byte, error) {
 	}
 
 	// 2xx
-	if response.StatusCode > 200 && response.StatusCode < 300 {
+	if response.StatusCode >= 200 && response.StatusCode < 300 {
 		return responseBody, nil
 	}
 	message := fmt.Sprintf("Status Code: %s - %s", strconv.Itoa(response.StatusCode), string(responseBody))
